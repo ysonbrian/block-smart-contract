f1c41e3af645fb94c276c253cb86c34a57f74b02
ysonbrian <znlsakxls@gmail.com>
2021-11-08 22:01:34 +0900

    First Contract

---
+++ contract/Counter.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Counter {
+  uint value;
+  function initialize (uint x) public {
+    value = x;
+  }
+
+  function get() view public returns (uint) {
+    return value;
+  }
+
+  function increment (uint n) public {
+    value = value + n;
+    // return (optional)
+  }
+
+  function decrement (uint n) public {
+    value = value - n;
+  }
+}
